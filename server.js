dc1543235c4f0a6c4ce0c39b6835607c7e55a12a
kiuvert39 <forrexplug@gmail.com>
2024-07-01 14:37:15 +0100

    not fount error

--- server.js
+++ server.js
@@ -45,10 +45,14 @@ app.get('/api/hello',async(req,res)=>{
         console.error('Error fetching data:', error.message);
         res.status(500).json({error})
     }   
+
   
 
 })
 
+app.use((req, res, next) => {
+    res.status(404).send('404 Not Found');
+  });
 
 app.listen(port,()=>{
     console.log(`listening at port ${port}`);
