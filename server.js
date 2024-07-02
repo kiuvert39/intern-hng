d81fbf54fccba9a880d2d55a5c1af6eecd945a96
Kliuvert Egbe <119463188+kiuvert39@users.noreply.github.com>
2024-07-02 08:44:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.get('/api/hello',async(req,res)=>{
         const response = await axios.get(`https://ipapi.co/${clientIp}/json`);
         const ip=response.data.ip
         const location=response.data.city
-        console.log(response.data);
         console.log('IPinfo response:', response.data.ip)
 
 
